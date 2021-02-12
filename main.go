565efb0bfcf7731fefa954768953d60642b126e1
mikifi <mikael@mikifi.net>
2021-02-12 09:37:25 +0100

    Adding expression as input parameter

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
         "github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-        Name string `json:"name"`
+type RpnExpression struct {
+        Expression string `json:"expression"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	var result, _ = ReversePolishNotation("2 2 +")
-	return fmt.Sprintf("Calc: %f!", result), nil
+func HandleRequest(ctx context.Context, input RpnExpression) (string, error) {
+	var result, _ = ReversePolishNotation(input.Expression)
+	return fmt.Sprintf("%f", result), nil
 }
 
 func main() {
